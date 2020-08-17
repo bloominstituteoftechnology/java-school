ffbcc12ea282cc35ff5805fa839db1a2d820cb98
oscar-lopez-projects <d1lopeztv@gmail.com>
2020-08-17 16:50:35 -0700

    finished the helperfunctions and implements

--- schools/src/main/java/com/lambdaschool/schools/exceptions/CustomErrorDetails.java
+++ schools/src/main/java/com/lambdaschool/schools/exceptions/CustomErrorDetails.java
@@ -1,2 +1,34 @@
-package com.lambdaschool.schools.exceptions;public class CustomErrorDetails {
+package com.lambdaschool.schools.exceptions;
+
+import com.lambdaschool.schools.services.HelperFunctions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+//this changes it from a pojo to a bean
+@Component
+public class CustomErrorDetails extends DefaultErrorAttributes {
+    @Autowired
+    HelperFunctions helperFunctions;
+
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+        Map<String, Object > errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+        Map<String, Object> errorDetails = new LinkedHashMap<>();
+        errorDetails.put("title", errorAttributes.get("error"));
+        errorDetails.put("status", errorAttributes.get("status"));
+        errorDetails.put("details", errorAttributes.get("message"));
+        errorDetails.put("timestamp", errorAttributes.get("timestamp"));
+
+        errorAttributes.put("developerMessage", "path " + errorAttributes.get("path"));
+
+        errorAttributes.put("errors", helperFunctions.getConstraintViolation(this.getError(webRequest)));
+
+
+        return errorDetails;
+    }
 }
