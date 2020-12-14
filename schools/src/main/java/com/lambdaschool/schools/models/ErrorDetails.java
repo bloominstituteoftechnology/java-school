062ceece57063b43cafefe13c3459be8af973173
Simone Ballard <simone.a.ballard@gmail.com>
2020-12-14 14:02:30 -0700

    Error Details added

---
+++ schools/src/main/java/com/lambdaschool/schools/models/ErrorDetails.java
@@ -0,0 +1,65 @@
+package com.lambdaschool.schools.models;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ErrorDetails {
+    private String title;
+    private int status;
+    private String details;
+    private Date timestamp;
+    private String developerMessage;
+    private List<ValidationError> errors = new ArrayList<>();
+
+    public ErrorDetails() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDeveloperMessage() {
+        return developerMessage;
+    }
+
+    public void setDeveloperMessage(String developerMessage) {
+        this.developerMessage = developerMessage;
+    }
+
+    public List<ValidationError> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<ValidationError> errors) {
+        this.errors = errors;
+    }
+}
