b33123a8394a3e492df3bd01f0853f8b36093ac4
Simone Ballard <simone.a.ballard@gmail.com>
2020-12-14 14:23:07 -0700

    Swagger2Config added

---
+++ schools/src/main/java/com/lambdaschool/schools/config/Swagger2Config.java
@@ -0,0 +1,45 @@
+package com.lambdaschool.schools.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+public class Swagger2Config
+{
+    @Bean
+    public Docket api()
+    {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors
+                .basePackage("com.lambdaschool.sampleemps"))
+            .paths(PathSelectors.regex("/.*"))
+            .build()
+            .apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo()
+    {
+        return new ApiInfoBuilder().title("Custom Swagger Documentation Example")
+            .description("Custom Swagger Documentation Example")
+            .contact(new Contact("John Mitchell",
+                "http://www.lambdaschool.com",
+                "john@lambdaschool.com"))
+            .license("MIT")
+            .licenseUrl("https://github.com/LambdaSchool/java-sampleswagger/blob/master/LICENSE")
+            .version("1.0.0")
+            .build();
+    }
+}
