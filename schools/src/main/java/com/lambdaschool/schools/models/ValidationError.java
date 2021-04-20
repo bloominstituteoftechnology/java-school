06defa5bb1bf199f22576e200c4363a583b870a4
eet349 <51262590+eet349@users.noreply.github.com>
2021-04-19 19:42:10 -0500

    Completes MVP.

---
+++ schools/src/main/java/com/lambdaschool/schools/models/ValidationError.java
@@ -0,0 +1,30 @@
+package com.lambdaschool.schools.models;
+
+public class ValidationError {
+    private String fieldName;
+    private String message;
+
+    public ValidationError() {
+    }
+
+    public ValidationError(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
